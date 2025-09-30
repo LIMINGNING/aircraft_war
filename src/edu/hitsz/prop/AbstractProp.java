60e35c50b84cd958d5fdb6479d2cbf1bc9c99ed9
MINGNING <2664135882@qq.com>
2025-09-30 11:16:50 +0800

    delete some useless code

--- src/edu/hitsz/prop/AbstractProp.java
+++ src/edu/hitsz/prop/AbstractProp.java
@@ -3,10 +3,6 @@ package edu.hitsz.prop;
 import edu.hitsz.aircraft.HeroAircraft;
 import edu.hitsz.application.Main;
 import edu.hitsz.basic.AbstractFlyingObject;
-import edu.hitsz.bullet.BaseBullet;
-
-import java.util.List;
-
 public abstract class AbstractProp extends AbstractFlyingObject {
     public AbstractProp(int locationX, int locationY, int speedX, int speedY) {
         super(locationX, locationY, speedX, speedY);
